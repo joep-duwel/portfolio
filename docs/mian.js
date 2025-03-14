0ea08094f5c77a7545928ed220e4db7fbae92e2a
joep-duwel <302287059@student.rocmondriaan.nl>
2025-03-14 12:56:35 +0100

    update

--- docs/mian.js
+++ docs/mian.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const typewriter = document.querySelector(".typewriter h1");
   typewriter.addEventListener("animationend", (event) => {
     if (event.animationName === "typing") {
-      // Verwijder de cursor na 3 seconden
+
       setTimeout(() => {
         typewriter.style.borderRight = "none"; // Verwijder de cursor
-      },0); // Na  seconden
+      },0); // Na  seconden 0
     }
   });
 });
